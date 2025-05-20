0d6e2b6ba5a5e70d6a49be07e7bc44f607c2dd8c
Justin Lacy <justin.m.lacy@gmail.com>
2025-05-20 07:44:43 -0700

    wip: audio yes/no options

--- electron/ffmpeg/slice.ts
+++ electron/ffmpeg/slice.ts
@@ -1,30 +1,35 @@
-import Ffmpeg from "fluent-ffmpeg";
 import type { MediaSlice } from "../../shared/edits";
 
 /**
  * select filter graph subtracks to final output tracks.
- * @param outName 
+ * @param audio - include audio output.
+ * @param outTrack -track names
  * @returns 
  */
-function mapOutput(outName: string = 'out') {
-	return ` -map [${outName}v] -map [${outName}a]`
+function mapOutput(outFile: string, audio?: boolean, outTrack: string = 'out') {
+	return ` -map [${outTrack}v]` + (audio ? `[${outTrack}a]` : '') + ` ${outFile}`;
 }
 
 /**
  * number of parts being concatenated
- * @param parts 
+ * @param slices 
  * @returns 
  */
-function makeConcat(parts: number, outname = 'out') {
+function makeConcat(slices: MediaSlice[], audio?: boolean, outTrack = 'out') {
 
-	// collect input track names.
-	let inputs = '';
-	for (let i = 0; i < parts; i++) {
-		inputs += `[${i}v][${i}a]`;
+	if (audio) {
+		// collect input track names.
+		const inputs = slices.map((_, i) => `[${i}v][${i}a]`).join('');
+
+		// concatenate into named output tracks.
+		return `${inputs}concat=n=${slices.length}:v=1:a=1[${outTrack}v][${outTrack}a]`;
+	} else {
+		// collect input track names.
+		const inputs = slices.map((_, i) => `[${i}v]`).join('');
+
+		// concatenate into named output tracks.
+		return `${inputs}concat=n=${slices.length}[${outTrack}v]`;
 	}
-
-	// concatenate into named output tracks.
-	return `${inputs}concat=n=${parts}:v=1:a=1[${outname}v][${outname}a]`;
 }
 
 /**
@@ -33,55 +38,41 @@ function makeConcat(parts: number, outname = 'out') {
  * @param outnum 
  * @returns 
  */
-function makeTrimPart(s: MediaSlice, outnum: number = 0) {
+function makeTrimPart(s: MediaSlice, outnum: number, audio?: boolean) {
 
 	///setpts=PTS=STARTPTS resets the frame count?
 	/// optional format change:
 	/// [0:v]trim=start=10.0:end=15.0,setpts=PTS-STARTPTS,format=yuv420p[0v];
-	// trim input a/v track 0, write numbered output a/v tracks
+	if (audio) {
 
-	//return `trim=start=${s.from}:end=${s.to},setpts=PTS-STARTPTS[${outnum}v];`;
+		// trim input a/v track 0, write numbered output a/v tracks
+		//separate A/V
+		const vid = `[0:v]trim=start=${s.from}:end=${s.to},setpts=PTS-STARTPTS[${outnum}v];`;
+		const aud = `[0:a]atrim=start=${s.from}:end=${s.to},asetpts=PTS-STARTPTS[${outnum}a];`;
+		return vid + aud;
 
-	//separate A/V
-	const vid = `[0:v]trim=start=${s.from}:end=${s.to},setpts=PTS-STARTPTS[${outnum}v];`;
-	const aud = `[0:a]atrim=start=${s.from}:end=${s.to},asetpts=PTS-STARTPTS[${outnum}a];`;
-	return vid + aud;
-
+	} else {
+		return `[0]trim=start=${s.from}:end=${s.to},setpts=PTS-STARTPTS[${outnum}v];`;
+	}
 
 }
 
-export async function sliceMedia(inUrl: string, slices: MediaSlice[], outUrl: string) {
-	let filter = '';
-	for (let i = 0; i < slices.length; i++) {
-		filter += makeTrimPart(slices[i], i);
-	}
-	filter += makeConcat(slices.length);
-	filter += mapOutput()
 
-	return new Promise<any>((res, rej) => {
-
-		//res(filter);
-
-		Ffmpeg(inUrl).addOption('-y')
-			//.complexFilter(filter)
-			.complexFilter(filter)
-			.on('end', () => {
-				res(null);
-			}).on('error', (error: Error) => {
-				rej(error);
-			}).output(outUrl);
-
-	});
-
+export function makeFilterInput(inUrl) {
+	return `ffmpeg -y -i ${inUrl} -filter_complex `
 }
 
-export async function buildSliceCmd(inUrl: string, slices: MediaSlice[], outUrl: string) {
-	let filter = '';
-	for (let i = 0; i < slices.length; i++) {
-		filter += makeTrimPart(slices[i], i);
-	}
-	filter += makeConcat(slices.length);
-	filter += mapOutput()
+export async function buildSliceCmd(
+	slices: MediaSlice[],
+	inUrl: string,
+	outUrl: string,
+	audio: boolean = true) {
+
+	let filter = makeFilterInput(inUrl);
+
+	filter += slices.map((s, i) => makeTrimPart(s, i, audio)).join('');
+	filter += makeConcat(slices, audio);
+	filter += mapOutput(outUrl, audio);
 
 	return filter;
 
--- test-ffmpeg.ts
+++ test-ffmpeg.ts
@@ -1,30 +1,9 @@
 import { execSync } from 'child_process';
 import path from "path";
-import { buildSliceCmd, sliceMedia } from "./electron/ffmpeg/slice";
+import { buildSliceCmd } from "./electron/ffmpeg/slice";
 
 
-async function test() {
-
-	try {
-
-		const inFile = './test.mp4';
-		const outFile = path.resolve('./', 'out.mp4');
-
-		await sliceMedia(inFile,
-			[
-				{ id: crypto.randomUUID(), from: 10, to: 20.5 }
-			],
-			outFile);
-
-	} catch (err) {
-		console.error(err);
-	}
-
-}
-
-
-testRaw();
-
+testRawAuto();
 
 /**
  * Test raw command.
@@ -33,20 +12,48 @@ async function testRaw() {
 
 	try {
 
-		const inFile = './test.mp4';
-		const outFile = path.resolve('./', 'out.mp4');
+		const inFile = './test.webm';
+		const outFile = path.resolve('./', 'out.webm');
 
-		const cmd = await buildSliceCmd(inFile,
+		const cmd = await buildSliceCmd(
 			[
 				{ id: crypto.randomUUID(), from: 10, to: 11.5 },
 				{ id: crypto.randomUUID(), from: 1, to: 2 }
 			],
+			inFile,
 			outFile);
 
 		const fullCmd = `ffmpeg -y -i ${inFile} -filter_complex ${cmd} ${outFile}`;
 		console.log(`${fullCmd}`);
 
 		execSync(fullCmd);
+
+	} catch (err) {
+		console.error(err);
+	}
+
+}
+
+
+/**
+ * Test raw command.
+ */
+async function testRawAuto() {
+
+	try {
+
+		const inFile = './test.mp4';
+		const outFile = path.resolve('./', 'out.mp4');
+
+		const cmd = await buildSliceCmd(
+			[
+				{ id: crypto.randomUUID(), from: 12, to: 14.5 },
+				{ id: crypto.randomUUID(), from: 1, to: 2 }
+			],
+			inFile,
+			outFile);
+
+		execSync(cmd);
 
 	} catch (err) {
 		console.error(err);
